8ad104aca85e814a7c7adb4791745b553b2b06e3
raja1802 <rajagandla16@gmail.com>
2024-01-20 20:30:03 +0530

    us and india stocks added

--- startv2.py
+++ startv2.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
 from apscheduler.schedulers.background import BackgroundScheduler
-from apscheduler.triggers.daily import DailyTrigger
+# from apscheduler.triggers.daily import DailyTrigger
 import re
 from datetime import datetime
 import pytz
